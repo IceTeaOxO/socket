f20fccab38d4de9b847a7453fe5de5d8edb4ad18
IceTeaOxO <popopuchang@gmail.com>
2022-11-21 19:36:10 +0800

    server

--- client.py
+++ client.py
@@ -1,27 +1,42 @@
 import socket
 import threading
+import time
+
+
+# 設定要連接的伺服器IP以及接口
+pars = ('127.0.0.1', 8888) 
+fileName = "good.html"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-
-pars = ('127.0.0.1', 8888) # 設定要連接的伺服器IP以及接口
 # 連接伺服器
 s.connect(pars)
+print("Connect:", str(pars))
 
-while True:
+#要求good.html檔案
+s.send(str(fileName).encode())
+data =  s.recv(1024).decode()
+print("From server:",data)
+# while True:
     
-    # 送出第一則訊息
-    s.send(b'request')
+#     # 送出第一則訊息
+#     s.send(b'request')
     
-    # 等待伺服器回應
-    data = s.recv(1024)
-    # 印出伺服器回應訊息
-    if data:
-        print("data from server:", data)
+#     # 等待伺服器回應
+#     data = s.recv(1024)
+#     # 印出伺服器回應訊息
+#     if data:
+#         print("data from server:", data)
 
-    # 結束連接
-    s.send(b'close')
-    break
-    
+#     # 結束連接
+#     s.send(b'close')
+#     break
+
+
+print("Receiving " + str(fileName), end ="...")
+time.sleep(3)
+s.send("<EXIT>".encode())
+
+s.send(b'close')
 # 關閉socket
 s.close()
